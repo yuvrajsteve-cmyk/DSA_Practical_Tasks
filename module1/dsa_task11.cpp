1adb298001bc032c199e9d4eee8449e13f3e2c5c
Yuvraj Singh <yuvrajsteve@email.com>
2026-08-12 11:09:31 +0530

    task 11: ii. Deletion at end

--- module1/dsa_task11.cpp
+++ module1/dsa_task11.cpp
@@ -13,6 +13,23 @@ struct Node {
 
 Node* head = NULL;
 
+void insertAtBeginning(int val) {
+    Node* newNode = new Node();
+    newNode->data = val;
+    newNode->next = head;
+    head = newNode;
+}
+
+void traverseList() {
+    Node* temp = head;
+    cout << "Your Train: ";
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
 void deleteFromBegining() {
     if (head == NULL) {
         cout << "This is also empty , nothing can be deleted! " << endl;
@@ -24,10 +41,41 @@ void deleteFromBegining() {
     delete temp;
 }
 
+void deleteFromEnd() {
+    if (head == NULL) {
+        return;
+    }
 
-int main () {
+    if (head->next == NULL) {
+        delete head;
+        head = NULL;
+        return;
+    }
+    
+    Node* temp = head;
+    while(temp->next->next != NULL) {
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+}
 
+int main() {
+    insertAtBeginning(10);
+    insertAtBeginning(20);
+    insertAtBeginning(30);
+    insertAtBeginning(40);
+    
+    cout << "--- Original Train ---" << endl;
+    traverseList(); 
+
+    cout << "\nDeleting from beginning..." << endl;
     deleteFromBegining();
+    traverseList(); 
+
+    cout << "\nDeleting from end..." << endl;
+    deleteFromEnd();
+    traverseList(); 
 
     return 0;
 }
