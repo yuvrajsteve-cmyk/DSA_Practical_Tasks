884e3f92fa94c6595c4262acbb64916d124c3e11
Yuvraj Singh <yuvrajsteve@email.com>
2026-08-15 15:12:36 +0530

    Task 15:Program to sort an array of integers in ascending order using selection sort.

--- Module 4/dsa_task15.cpp
+++ Module 4/dsa_task15.cpp
@@ -3,29 +3,33 @@
 #include <iostream>
 using namespace std;
 
-int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 121};
-int low = 0;
-int high = 10;
-int target = 121;
-
 int main () {
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
 
-        if (arr[mid] == target) {
-            int result = mid;
-            cout << "Found it: " << result << endl;
-            break;
-        } else if (target < arr[mid]) {
-            high = mid - 1;
-        }else {
-            low = mid + 1;
+    int arr[] = {20, 10, 50, 12, 30, 23, 26, 56, 43, 654, 345, 344, 344};
+    int n = 13;
+
+    for (int i = 0; i < n - 1; i++) {
+        
+        int min_idx = i;
+        cout << min_idx << endl;
+
+        for (int j = i + 1; j < n; j ++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
         }
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
     }
 
-    if (low > high) {
-        cout << "Element not Exist!";
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
+
+    cout << endl;
+
 
     return 0;
 }
